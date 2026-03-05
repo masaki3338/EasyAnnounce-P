e7b78d503596c730535b4f04bb6eb3e4e0cade45
masaki3338 <masakicarp@jcom.home.ne.jp>
2026-03-05 09:27:53 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ import VersionInfo from "./screens/VersionInfo";
 
 
 // バージョン番号を定数で管理
-const APP_VERSION = "0.23 β"
+const APP_VERSION = "1.00 β"
 
 // iOS 判定を共通で使えるようにグローバル定数として定義
 const isIOS = (() => {
