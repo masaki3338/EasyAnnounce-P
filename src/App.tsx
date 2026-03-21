c38c35c13a53e0ceb4d36ca98f443bb914535581
masaki3338 <masakicarp@jcom.home.ne.jp>
2026-03-21 18:29:33 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -587,7 +587,7 @@ const handleSpeak = async () => {
             className="m-4 px-4 py-2 bg-gray-200 rounded-full shadow-sm hover:bg-gray-300 transition"
             onClick={() => setScreen("menu")}
           >
-            ← メニューに戻る
+            メニュー
           </button>
           <AnnounceMindset />
         </>
