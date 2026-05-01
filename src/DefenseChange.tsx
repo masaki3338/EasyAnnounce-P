32dab2e0a3394c2a5bb14c0ae5e42fd659fd105b
masaki3338 <masakicarp@jcom.home.ne.jp>
2026-05-01 11:20:19 +0900

    Add files via upload

--- src/DefenseChange.tsx
+++ src/DefenseChange.tsx
@@ -8907,7 +8907,7 @@ const canDropHere =
             <div className="px-5 py-4 border border-red-500 bg-red-200 text-red-700 rounded-xl">
               <div
                 ref={modalTextRef}
-                className="text-rose-600 text-[clamp(28px,4.8vw,46px)] leading-[1.45] font-bold whitespace-pre-wrap text-center"
+                className="text-rose-600 text-[clamp(20px,3.2vw,30px)] leading-[1.45] font-bold whitespace-pre-wrap text-center"
                 dangerouslySetInnerHTML={{ __html: announcementText.displayText }}
               />
 
